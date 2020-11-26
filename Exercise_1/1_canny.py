6e420b92d4097d93bd46fbb7ad0368c4e79893cd
xing chunwei <xingcw602@sina.com>
2020-11-27 00:03:47 +0100

    lab session 1 first version, to be checked.

--- Exercise_1/1_canny.py
+++ Exercise_1/1_canny.py
@@ -1,49 +1,78 @@
 import numpy as np
-from scipy.ndimage.filters import gaussian_filter,convolve
+from scipy.ndimage.filters import gaussian_filter, convolve
 import matplotlib.pyplot as plt
 from scipy import where
 import cv2
+import itertools
+
+
+def get_neighbors(i, j, h, w, hw, direction):
+    neighbors = []
+    if direction == 0:
+        neighbors = [[i, jloc] for jloc in range(j-hw, j+hw)]
+    elif direction == 1:
+        neighbors = [[iloc, j+iloc-i] for iloc in range(i-hw, i+hw)]
+    elif direction == 2:
+        neighbors = [[iloc, j] for iloc in range(i - hw, i + hw)]
+    elif direction == 3:
+        neighbors = [[iloc, j - iloc + i] for iloc in range(i - hw, i + hw)]
+    neighbors_c = neighbors.copy()
+    for t in neighbors_c:
+        if t[0] not in range(0, w) or t[1] not in range(0, h):
+            neighbors.remove(t)
+    return neighbors
 
 ### Read Image ###
-im=cv2.imread('/home/cvcourse/pics/zurlim.png', 0).astype('float')
-
+im = cv2.imread('zurlim.png', 0).astype('float')
 
 ####### Gaussian Smooth Image #######
 # TODO: Implement Gaussian Smooting
 # Useful functions: gaussian_filter
-#blurred_im = ...
-
+blurred_im = gaussian_filter(im, sigma=2.0)
 
 ###### Gradients x and y (Sobel filters) ######
 # TODO: Implement Gradient along x and y
 # Useful functions: convolve
 
-#im_x = ... 
-#im_y = ...
-
+im_x = convolve(blurred_im, np.array([[-1, 0, 1],
+                                      [-2, 0, 2],
+                                      [-1, 0, 1]]))
+im_y = convolve(blurred_im, np.array([[-1, -2, -1],
+                                      [0, 0, 0],
+                                      [1, 2, 1]]))
 
 ###### gradient and direction ########
 # TODO: Implement Gradient Magnitude and Direction
 
-#gradient = ...
-#theta = ...
+gradient = np.power((np.power(im_x, 2) + np.power(im_y, 2)), 0.5)
+theta = np.arctan(im_y / im_x)
+theta[np.isnan(theta)] = 0.0
 
-
-thresh=30;
+thresh = 30
 # TODO: Implement Thresholding criteria
-#thresholdEdges = ...
-
+thresholdEdges = blurred_im
+thresholdEdges[gradient >= thresh] = 255
+thresholdEdges[gradient < thresh] = 0
 
 # TODO: Implement Non-Maximum Suppression
 ###### Convert to degree ######
-#theta = ...
+theta = theta / np.pi * 180
 ###### Quantize angles ######
-#
+theta_c = theta.copy()
+theta[theta_c < -45] = 0
+theta[(theta_c < 0) & (theta_c >= -45)] = 1
+theta[(theta_c < 45) & (theta_c >= 0)] = 2
+theta[(theta_c <= 90) & (theta_c >= 45)] = 3
 ###### Non-maximum suppression ########
-### ...
-### ...
-#edges= ...
-
+halfwidth = 2
+w, h = im.shape[:2]
+for i in range(w):
+    for j in range(h):
+        n_list = get_neighbors(i, j, h, w, hw=halfwidth, direction=theta[i][j])
+        neighbors = gradient[tuple(np.asarray(n_list).T.tolist())]
+        if gradient[i][j] != np.max(neighbors):
+            thresholdEdges[i][j] = 0
+edges = thresholdEdges
 
 # Plotting of results
 # No need to change it
@@ -56,4 +85,3 @@ ax_arr[0].imshow(im, cmap='gray')
 ax_arr[1].imshow(edges, cmap='gray')
 plt.show()
 plt.pause(5)
-
--- Exercise_1/1_harris.py
+++ Exercise_1/1_harris.py
@@ -1,11 +1,24 @@
 from matplotlib import pyplot as plt
 from scipy.ndimage.interpolation import rotate
 from scipy.ndimage.filters import gaussian_filter1d, gaussian_filter
+from scipy.signal import convolve2d
 import numpy as np
 import cv2
+import itertools
 
 
 # Implement a function that performs non-maximum suppression. You can leave it for the end
+def get_neighbors(i, j, h, w, hw):
+    neighbors = list(itertools.product(range(i - hw, i + hw), range(j - hw, j + hw)))
+    neighbors_c = neighbors.copy()
+    for t in neighbors_c:
+        if t[0] not in range(0, w) or t[1] not in range(0, h):
+            neighbors.remove(t)
+    x = np.unique(np.asarray([t[0] for t in neighbors]))
+    y = np.unique(np.asarray([t[1] for t in neighbors]))
+    neighbors = [x.tolist(), y.tolist()]
+    return neighbors
+
 
 def nonmax_suppression(harris_resp, thr, halfwidth=2):
     # Outputs:
@@ -16,7 +29,16 @@ def nonmax_suppression(harris_resp, thr, halfwidth=2):
     cornersy = []
     cornersx = []
     h, w = im.shape[:2]
+
     # TODO: perform non-maximum suppression
+
+    for i in range(w):
+        for j in range(h):
+            n_list = get_neighbors(i, j, h, w, hw=halfwidth)
+            neighbors = harris_resp[np.ix_(n_list[0], n_list[1])]
+            if harris_resp[i][j] >= thr and harris_resp[i][j] == np.max(neighbors):
+                cornersx.append(i)
+                cornersy.append(j)
 
     return cornersy, cornersx
 
@@ -27,11 +49,11 @@ def nonmax_suppression(harris_resp, thr, halfwidth=2):
 sigma_w = 2.0
 sigma_d = 2.0
 kappa = 0.04
-rot_angle = 0
+rot_angle = 60
 thresh = 800
 
 # Read the image
-im = cv2.imread('/home/cvcourse/pics/CircleLineRect.png', 0)
+im = cv2.imread('test.png', 0)
 im = im.astype('float')
 
 # Rotation of the image
@@ -41,19 +63,31 @@ if rot_angle != 0:
 # TODO: Implement Harris corners
 # Useful functions: gaussian_filter1d, gaussian_filter
 
-# I_x = ...
-# I_y = ...
-# .........
-# H = ...
-corn = nonmax_suppression(H,thresh,2)
+# im_x = gaussian_filter1d(im, axis=0, sigma=sigma_d, order=1)
+# im_y = gaussian_filter1d(im, axis=1, sigma=sigma_d, order=1)
 
+I_x = convolve2d(im, np.array([[-1, 0, 1],
+                               [-2, 0, 2],
+                               [-1, 0, 1]]))
+I_y = convolve2d(im, np.array([[-1, -2, -1],
+                               [0, 0, 0],
+                               [1, 2, 1]]))
+I_x2_bar = gaussian_filter(np.multiply(I_x, I_x), sigma=sigma_w)
+I_y2_bar = gaussian_filter(np.multiply(I_y, I_y), sigma=sigma_w)
+I_xy_bar = gaussian_filter(np.multiply(I_x, I_y), sigma=sigma_w)
+H = np.zeros(shape=(im.shape[0], im.shape[1]))
+for i in range(im.shape[0]):
+    for j in range(im.shape[1]):
+        A = np.asarray([[I_x2_bar[i][j], I_xy_bar[i][j]], [I_xy_bar[i][j], I_y2_bar[i][j]]])
+        H[i][j] = np.linalg.det(A) - kappa * (np.multiply(np.trace(A), np.trace(A)))
+corn = nonmax_suppression(H, thresh, 5)
 
 # Visualization of the results
 
 # Plotting of results
 # No need to change it
 plt.close("all")
-plt.ion()
+# plt.ion()
 f, ax_arr = plt.subplots(1, 3, figsize=(18, 16))
 ax_arr[0].set_title("Input Image")
 ax_arr[1].set_title("Harris Response")
@@ -61,9 +95,6 @@ ax_arr[2].set_title("Detections")
 ax_arr[0].imshow(im, cmap='gray')
 ax_arr[1].imshow(H, cmap='gray')
 ax_arr[2].imshow(im, cmap='gray')
-ax_arr[2].scatter(x=corn[1], y=corn[0], c='r', s=10)
+ax_arr[2].scatter(x=corn[0], y=corn[1], c='r', s=10)
 plt.show()
-plt.pause(5)
-
-
-
+# plt.pause(5)
